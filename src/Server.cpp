795c5c0a15bd37a3a3d34708c3aebfb955eaa2e9
Rui Yi Gan <kurtganruiyi@gmail.com>
2025-02-01 12:21:30 +0800

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,6 +7,8 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/event.h>
+#include <sys/time.h>
 
 int main(int argc, char **argv)
 {
@@ -48,43 +50,76 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    struct sockaddr_in client_addr;
-    int client_addr_len = sizeof(client_addr);
-    std::cout << "Waiting for a client to connect...\n";
+    // Creates the event queue in the kernel and returns the file descriptor referencing it
+    // kq is what we use for all sub operations to tell the kernelt what we want to monitor and to retrieve which events that occured
+    // Like a mailbox with subs (file descriptors to watch) and pick up mail (events that happened)
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!\n";
-
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-
-    std::cout << "Client connected\n";
-
-    char buffer[1024];
-    int bytes_recv;
-
-    while (true)
-    {
-        bytes_recv = recv(client_fd, buffer, sizeof(buffer) - 1, 0); // Receive up to 1023 bytes
-        if (bytes_recv > 0)
-        {
-            buffer[bytes_recv] = '\0'; // Null-terminate received data
-            std::cout << "Received: " << buffer << std::endl; // Print received data
-            char *msg = "+PONG\r\n";
-            int msg_len = strlen(msg);
-
-            send(client_fd, msg, msg_len, 0);
-        }
-        else if (bytes_recv == 0)
-        {
-            std::cout << "Client disconnected." << std::endl;
-            break;
-        }
-        else
-        {
-            std::cout << "Error in recv - " << std::endl;
-            break;
-        }
+    int kq = kqueue();
+    if (kq == -1) {
+        std::cerr << "Creation of kqueue failed\n";
+        return 1;
     }
+
+    struct kevent change; // the thing we want to watch/monitor
+    EV_SET(&change, server_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, nullptr); // can view as init
+    kevent(kq, &change, 1, nullptr, 0, nullptr);
+
+    struct kevent event; // receiving the event
+
+    while (true) {
+        int num_events = kevent(kq, nullptr, 0, &event, 1, nullptr); // also like init
+        if (num_events < 0)
+        {
+            std::cerr << "epoll_wait failed\n";
+            break; // You might want more graceful handling
+        }
+
+        if (event.ident == server_fd && event.filter == EVFILT_READ) {
+            struct sockaddr_in client_addr;
+            int client_addr_len = sizeof(client_addr);
+            std::cout << "Waiting for a client to connect...\n";
+
+            // You can use print statements as follows for debugging, they'll be visible when running tests.
+            std::cout << "Logs from your program will appear here!\n";
+
+            int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+
+            std::cout << "Client connected\n";
+
+            // Subscribe client socket 
+            EV_SET(&change, client_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, nullptr); // can view as init
+            kevent(kq, &change, 1, nullptr, 0, nullptr);
+        } else {
+            // This should be client socket 
+            int fd = static_cast<int>(event.ident);
+            
+            char buffer[1024];
+            int bytes_recv;
+
+            bytes_recv = recv(fd, buffer, sizeof(buffer) - 1, 0); // Receive up to 1023 bytes
+            if (bytes_recv > 0)
+            {
+                buffer[bytes_recv] = '\0'; // Null-terminate received data
+                std::cout << "Received: " << buffer << std::endl; // Print received data
+                char *msg = "+PONG\r\n";
+                int msg_len = strlen(msg);
+
+                send(fd, msg, msg_len, 0);
+            }
+            else if (bytes_recv == 0)
+            {
+                std::cout << "Client disconnected." << std::endl;
+                break;
+            }
+            else
+            {
+                std::cout << "Error in recv - " << std::endl;
+                break;
+            }
+        }
+
+    }
+
 
     close(server_fd);
 
