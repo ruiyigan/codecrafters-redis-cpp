f74bd41d7b400a510c576c54fd76239bbbd05f9a
Rui Yi Gan <kurtganruiyi@gmail.com>
2025-02-04 17:36:41 +0800

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -74,14 +74,14 @@ private:
 
                         auto it = storage_->find(key);
                         if (it == storage_->end()) {
-                            message = "-1";
+                            message = "$-1\r\n";
                         } else {
                             std::string stored_value = std::get<0>(it -> second);
                             std::time_t expiry_time = std::get<1>(it -> second);
 
                             if (expiry_time != 0 && std::time(nullptr) > expiry_time) {
                                 storage_->erase(it);
-                                message = "-1";
+                                message = "$-1\r\n";
                             } else {
                                 message = stored_value;
                             }
